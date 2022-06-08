0f304f0fcc783a34078b50fddd020da76d874e65
jihyun <ziihyun321@gmail.com>
2022-06-08 19:37:43 +0900

    feat. Add connect changed account when change metamask account

--- dapp/src/ContractPage.js
+++ dapp/src/ContractPage.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import { ethers } from 'ethers';
+import {ethers} from 'ethers';
 import Contract_abi from './Contract_abi.json';
 
 function ContractPage() {
-    const contractAddress = '0xCb867CA1Cd09779D74e8c1F707c02d5285E0De44';    
+    const contractAddress = '0xCb867CA1Cd09779D74e8c1F707c02d5285E0De44';
     const [errorMessage, setErrorMessage] = useState(null);
     const [defaultAccount, setDefaultAccount] = useState(null);
     const [connButtonText, setConnButtonText] = useState('Connect Wallet');
@@ -14,16 +14,33 @@ function ContractPage() {
     const [signer, setSigner] = useState(null);
     const [contract, setContract] = useState(null);
 
+    useEffect(() => {
+        console.log('=== useEffect ===');
+        // 메타마스크 다른 account 연결시, account 재연결 
+        window
+            .ethereum
+            .on('accountsChanged', function (accounts) {
+                console.log('accounts[0]: ', accounts[0]);
+                accountChangedHandler(accounts[0]);
+                // Time to reload your interface with accounts[0]!
+            });
+    })
+
     // 필요한 버튼 누르면 해당 계정에 들어가도록 함
     const connectWalletHandler = () => {
+        console.log('=== connectWalletHandler ===');
+
         // 창에 메타마스크가 있는지 확인
         if (window.ethereum) {
             // 메타마스크에 해당 계정 요청
-            window.ethereum.request({method: 'eth_requestAccounts'})
-            .then(result => {
-                setConnButtonText('Wallet Connected!');
-                accountChangedHandler(result[0]);
-            })
+            window
+                .ethereum
+                .request({method: 'eth_requestAccounts'})
+                .then(result => {
+                    setConnButtonText('Wallet Connected!');
+                    console.log('result[0]: ', result[0]);
+                    accountChangedHandler(result[0]);
+                })
         } else {
             // 메타마스크가 없으면 다음 에러 띄어줌
             setErrorMessage('메타마스크 설치 필요');
@@ -38,17 +55,23 @@ function ContractPage() {
     const updateEthers = () => {
         console.log('=== updateEthers ===');
 
-        // MetaMask injects a Web3 Provider as "web3.currentProvider", so
-        // we can wrap it up in the ethers.js Web3Provider, which wraps a
-        // Web3 Provider and exposes the ethers.js Provider API.
-        let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+        // MetaMask injects a Web3 Provider as "web3.currentProvider", so we can wrap it
+        // up in the ethers.js Web3Provider, which wraps a Web3 Provider and exposes the
+        // ethers.js Provider API.
+        let tempProvider = new ethers
+            .providers
+            .Web3Provider(window.ethereum);
         setProvider(tempProvider);
 
         // There is only ever up to one account in MetaMask exposed
         let tempSigner = tempProvider.getSigner();
         setSigner(tempSigner);
 
-        let tempContract = new ethers.Contract(contractAddress, Contract_abi, tempSigner);
+        let tempContract = new ethers.Contract(
+            contractAddress,
+            Contract_abi,
+            tempSigner
+        );
         setContract(tempContract);
 
         console.log('tempProvider: ', provider);
@@ -59,10 +82,16 @@ function ContractPage() {
     const getCurrentVal = async () => {
         updateEthers();
 
-        let val = await contract.balanceOf(contractAddress);
-        console.log('val: ', val);
-        // // let val = await contract.get();
-        // setCurrentContractVal(val);
+        const balance = await provider.getBalance(contractAddress);
+        let balance2 = ethers
+            .utils
+            .formatEther(balance);
+        console.log('balance: ', balance);
+        console.log('balance2: ', balance2);
+
+        // let val = await contract.balanceOf(contractAddress); console.log('val: ',
+        // val); console.log('val: ', val._hex); setCurrentContractVal(val);  let val =
+        // await contract.get(); setCurrentContractVal(val);
     }
 
     const handleTransfer = (event) => {
@@ -70,30 +99,90 @@ function ContractPage() {
         console.log('보내는 주소: ', event.target.sendAddress.value);
         console.log('보내는 양: ', event.target.sendAmount.value);
 
-        contract.transfer(event.target.sendAddress.value, event.target.sendAmount.value);
+        contract.transfer(
+            event.target.sendAddress.value,
+            event.target.sendAmount.value
+        );
         // contract.set(event.target.setText.value);
     }
 
-    return (<div>
-        <h3> {"Get/Set Interaction with contract!"} </h3>
-        <button onClick={connectWalletHandler}>{connButtonText}</button>
-        <h3> Address: {defaultAccount} </h3>
-        
-        <form onSubmit={handleTransfer}>
-            <div>
-                To: <input id='sendAddress' type='text'></input>
+    return (
+        <div className='mt-10'>
+            <h3 className='text-3xl font-bold'>
+                {"Interaction with contract!"}
+            </h3>
+            <div className='mt-3'>
+                <button
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                    onClick={connectWalletHandler}>{connButtonText}</button>
+                <button
+                    className='ml-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+                    onClick={getCurrentVal}>Get Current Value</button>
+                <h3 className='mt-3'>
+                    Address: {defaultAccount}
+                </h3>
+                <div className='mt-3'>
+                    balanceOf: {currentContractVal}
+                </div>
             </div>
-            <div>
-                Amount: <input id='sendAmount' type='text'></input>
-            </div>                
-            <button type='submit'>Transfer</button>
-        </form>
 
-        {/* 가져오기 전에 무조건 setHandler 눌러서 설정해야함 */}
-        <button onClick={getCurrentVal}>Get Current Value</button>
-        {currentContractVal}
-        {errorMessage}
-    </div>)
+            <div className='mt-5'>
+                <div className='text-2xl font-bold'>Trnasfer Test</div>
+                <form className='' onSubmit={handleTransfer}>
+                    <div className='mt-3'>
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="sendAddress">
+                                    Send To
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="sendAddress"
+                                    type="text"
+                                    placeholder="0x0"/>
+                            </div>
+                        </div>
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label
+                                    class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    for="sendAmount">
+                                    Amount
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-96 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="sendAmount"
+                                    type="text"
+                                    placeholder="100.."/>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <div>
+                        To:
+                        <input id='sendAddress' type='text'></input>
+                    </div>
+                    <div>
+                        Amount:
+                        <input id='sendAmount' type='text'></input>
+                    </div> */
+                    }
+                    <button
+                        className='shadow bg-cyan-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+                        type='submit'>Transfer</button>
+                </form>
+            </div>
+
+            {currentContractVal}
+            {errorMessage}
+        </div>
+    )
 }
 
 export default ContractPage
